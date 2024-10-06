5d70ed5876d52549d9da2d1d16cbf74f1efb8605
Manavsharma7017 <manavsharma7017@gmail.com>
2024-10-06 19:37:25 +0530

    nagivation commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,15 @@
-import {Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-export default function foryou(){
-    return<SafeAreaView>
-        <Text>for you</Text>
-    </SafeAreaView>
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Suggested from '../foryoutopbar/Suggested';
+import Library from '../foryoutopbar/Library';
+import Liked from '../foryoutopbar/Liked';
+const Tab = createMaterialTopTabNavigator();
+
+export default function foryou() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Library" component={Library} />
+      <Tab.Screen name="Liked" component={Liked} />
+      <Tab.Screen name='Suggested' component={Suggested}/>
+    </Tab.Navigator>
+  );
 }
